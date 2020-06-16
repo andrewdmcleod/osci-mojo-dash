be2acbf427d4bcbcae7c2b715e8a5f0f98e25b99
andrew mcleod <andrew.mcleod@canonical.com>
2020-06-16 16:11:03 +0000

    add ubuntu_lite

--- get_mojo_results.py
+++ get_mojo_results.py
@@ -9,6 +9,9 @@ import copy
 
 os.environ['http_proxy'] = ''
 
+class E(Exception):
+    pass
+
 numBuilds = 100
 valid_configs = [
 'trusty-mitaka',
@@ -24,11 +27,18 @@ valid_configs = [
 'eoan-train'
 ]
 
+valid_func_configs = [
+'trusty-mitaka',
+'xenial-mitaka',
+'bionic-queens'
+]
+
 all_urls = { 'os_upgrade': "http://osci:8080/view/MojoMatrix/job/test_mojo_openstack_upgrade_master_matrix/MOJO_SPEC=specs%2Ffull_stack%2Fnext_openstack_upgrade,U_OS=",
             'charm_upgrade': "http://osci:8080/view/MojoMatrix/job/test_mojo_charm_upgrade_ha_matrix/MOJO_SPEC=specs%2Ffull_stack%2Fstable_to_next_ha,U_OS=",
             'designate_ha': "http://osci:8080/view/MojoMatrix/job/test_mojo_designate_ha_master_matrix/MOJO_SPEC=specs%2Ffull_stack%2Fnext_designate_ha,U_OS=",
             'vrrp_ha': "http://osci:8080/view/MojoMatrix/job/test_mojo_vrrp_ha_master_matrix/MOJO_SPEC=specs%2Ffull_stack%2Fnext_ha_vrrp,U_OS=",
-            'series_upgrade': "http://osci:8080/view/MojoMatrix/job/test_mojo_series_upgrade_master_matrix/MOJO_SPEC=specs%2Ffull_stack%2Fnext_series_upgrade,U_OS="
+            'series_upgrade': "http://osci:8080/view/MojoMatrix/job/test_mojo_series_upgrade_master_matrix/MOJO_SPEC=specs%2Ffull_stack%2Fnext_series_upgrade,U_OS=",
+            'ubuntu_lite': "http://osci:8080/view/FuncMatrix/job/test_func_series_upgrade_ubuntu_lite_matrix/TEST_PATH=development%2Fubuntu-lite-series-upgrade-"
            }
 
 count = 1 
@@ -42,7 +52,6 @@ configss = {}
 for cur_url in all_urls.items():
     configss = {}
     matrix = {}
-    print(cur_url[1])
     for current_config in valid_configs:
         count += 1
         lastline = ""
@@ -52,16 +61,45 @@ for cur_url in all_urls.items():
             sortby = 1
         if "bionic" in current_config:
             sortby = 2
+        if "test_func_" in cur_url[1]:
+            func = True
+            func_config = current_config.split("-")[0]
+        else:
+            func = False
         try:
-            with urllib.request.urlopen("{}{}/lastBuild/api/json/".format(cur_url[1],current_config)) as jsonurl:
+            if func:
+                if current_config not in valid_func_configs:
+                    print("{} not in valid_func_configs".format(current_config))
+                    raise
+                get_url = "{}{}/lastBuild/api/json/".format(cur_url[1],func_config)
+                split_chr = '-'
+                split_num = 4 
+            else:
+                get_url = "{}{}/lastBuild/api/json/".format(cur_url[1],current_config)
+                split_chr = '='
+                split_num = 2 
+            with urllib.request.urlopen(get_url) as jsonurl:
                 jsdata = json.loads(jsonurl.read().decode())
                 timestamp=jsdata['timestamp']
                 displayName=jsdata['fullDisplayName']
-                url=cur_url[1] + current_config + "/lastBuild/"
+                if func:
+                    url=cur_url[1] + func_config + "/lastBuild/"
+                else:
+                    url=cur_url[1] + current_config + "/lastBuild/"
                 result=jsdata['result']
                 specName=cur_url[0]
-                config=jsdata['url'].split('/')[7].split('=')[2]
+                print(jsdata['url'])
+                config=jsdata['url'].split('/')[7].split(split_chr)[split_num]
+                if func:
+                    config = current_config
                 datetime = time.strftime('%Y-%m-%d', time.gmtime(jsdata['timestamp'] / 1000))
+        except urllib.error.HTTPError:
+            config=current_config
+            timestamp="0"
+            datetime="0"
+            result="EMPTY"
+            url="null"
+            specName=cur_url[0]
         except:
             config=current_config
             timestamp="0"
@@ -69,6 +107,7 @@ for cur_url in all_urls.items():
             result="EMPTY"
             url="null"
             specName=cur_url[0]
+ 
         if "vrrp" in url and "trusty-mitaka" in url:
             result="EMPTY"
         if config in valid_configs:
@@ -85,8 +124,11 @@ for cur_url in all_urls.items():
                       'specName': specName,
                       'config': configss,
                       }}
+            print("Updating matrix: {}".format(matrix))
             matrix_full.update(matrix)
             filename = "mojospecs_output.json".format(specName)
+        else:
+            print("{} not found ".format(config))
 
 
 def sortit(d):
